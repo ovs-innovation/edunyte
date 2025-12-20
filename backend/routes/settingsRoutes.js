af23d2db296801c6ee24fc72b62b1aa61d7f0af9
Angadkumar07 <angadkumar70676@gmail.com>
2025-12-20 13:00:33 +0530

    fixed changes

--- backend/routes/settingsRoutes.js
+++ backend/routes/settingsRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { getSettings, updateSettings } from "../controllers/settingsController.js";
 import { verifyToken } from "../middlewares/authMiddleware.js";
+import { requirePermission } from "../middlewares/permissionMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", verifyToken, getSettings);
-router.patch("/", verifyToken, updateSettings);
+router.get("/", verifyToken, requirePermission("settings.view"), getSettings);
+router.patch("/", verifyToken, requirePermission("settings.edit"), updateSettings);
 
 export default router;
 
