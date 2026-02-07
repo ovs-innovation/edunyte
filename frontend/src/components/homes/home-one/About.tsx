91259f7692dbb9cadc238679e75fbf8fc70dcfaf
Angadkumar07 <angadkumar70676@gmail.com>
2026-02-07 18:28:24 +0530

    content updated about-us section

--- frontend/src/components/homes/home-one/About.tsx
+++ frontend/src/components/homes/home-one/About.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 import VideoPopup from "../../../modals/VideoPopup";
 import BtnArrow from "../../../svg/BtnArrow";
 
 const About = () => {
-
+   const { t } = useTranslation();
    const [isVideoOpen, setIsVideoOpen] = useState(false);
 
    return (
@@ -22,7 +23,7 @@ const About = () => {
                            </svg>
                         </a>
                         <div className="about__enrolled" data-aos="fade-right" data-aos-delay="200">
-                           <p className="title"><span>36K+</span> Enrolled Students</p>
+                           <p className="title"><span>37K+</span> {t('common.enrolled_students')}</p>
                            <img src="/assets/img/others/student_grp.png" alt="img" />
                         </div>
                      </div>
@@ -31,36 +32,35 @@ const About = () => {
                   <div className="col-lg-6">
                      <div className="about__content">
                         <div className="section__title">
-                           <span className="sub-title">Get More About Us</span>
+                           <span className="sub-title">{t('common.about_subtitle')}</span>
                            <h2 className="title">
-                              Thousand Of Top
+                              {t('common.about_title_1')}
                               <span className="position-relative title-shape-wrap">
                                  <svg x="0px" y="0px" preserveAspectRatio="none" viewBox="0 0 209 59" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M4.74438 7.70565C69.7006 -1.18799 136.097 -2.38304 203.934 4.1205C207.178 4.48495 209.422 7.14626 208.933 10.0534C206.793 23.6481 205.415 36.5704 204.801 48.8204C204.756 51.3291 202.246 53.5582 199.213 53.7955C136.093 59.7623 74.1922 60.5985 13.5091 56.3043C10.5653 56.0924 7.84371 53.7277 7.42158 51.0325C5.20725 38.2627 2.76333 25.6511 0.0898448 13.1978C-0.465589 10.5873 1.61173 8.1379 4.73327 7.70565" fill="currentcolor" />
                                  </svg>
-                                 Courses
+                                 {t('common.about_title_highlight')}
                               </span>
-                              Now in One Place
+                              , {t('common.about_title_2')}
                            </h2>
                         </div>
-                        <p className="desc">Groove’s intuitive shared inbox makes it easy for team members to
-                           organize, prioritize and.In this episode of the Smashing Pod we’re talking about Web Platform Baseline.</p>
+                        <p className="desc">{t('common.about_description')}</p>
                         <ul className="about__info-list list-wrap">
                            <li className="about__info-list-item">
                               <i className="flaticon-angle-right"></i>
-                              <p className="content">The Most World Class Instructors</p>
+                              <p className="content">{t('common.about_list_1')}</p>
                            </li>
                            <li className="about__info-list-item">
                               <i className="flaticon-angle-right"></i>
-                              <p className="content">Access Your Class anywhere</p>
+                              <p className="content">{t('common.about_list_2')}</p>
                            </li>
                            <li className="about__info-list-item">
                               <i className="flaticon-angle-right"></i>
-                              <p className="content">Flexible Course Plan</p>
+                              <p className="content">{t('common.about_list_3')}</p>
                            </li>
                         </ul>
                         <div className="tg-button-wrap">
-                           <Link to="/about-us" className="btn arrow-btn">Start Free Trial <BtnArrow /></Link>
+                           <Link to="/contact" className="btn arrow-btn">{t('common.start_free_trial')} <BtnArrow /></Link>
                         </div>
                      </div>
                   </div>
